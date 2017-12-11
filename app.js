4c0e1a49f1d0f5c2460b2146a393e0ca46772445
David McDeavitt <david.mcdeavitt@au.tribalworldwide.com>
2017-12-11 15:59:48 +1100

    test ajax

---
+++ app.js
@@ -0,0 +1,14 @@
+var $;
+$ = require('jquery');
+
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "http://test.stage.tribalddbmelb.com.au/people.json", true);
+  xhttp.send();
+}
